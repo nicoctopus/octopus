630190b2f9a4018a08fb8e86bfd2686ac197b490
RobinF <robin.flecchia@gmail.com>
2012-03-06 16:45:09 +0100

    Mouvement des formes sur le blackBoard

--- view/ellipseduprojet.h
+++ view/ellipseduprojet.h
@@ -1,6 +1,8 @@
 #ifndef ELLIPSEDUPROJET_H
 #define ELLIPSEDUPROJET_H
 
+#include "blackboard.h"
+class BlackBoard;
 #include<QGraphicsEllipseItem>
 #include <QGraphicsScene>
 #include "model/movement.h"
@@ -10,19 +12,30 @@ class EllipseDuProjet : public QGraphicsEllipseItem
    enum { Type = UserType + 1 };
 
 public:
-    EllipseDuProjet(qreal x, qreal y, qreal width, qreal height, QColor* color, QGraphicsScene *scene, Movement *movement);
+    EllipseDuProjet(qreal x, qreal y, qreal width, qreal height, QColor* color, QGraphicsScene *scene, Movement *movement, BlackBoard* blackboard);
     Movement* getMovement();
    int type() const
     {
         return Type;
     }
-
    bool getContextMenu();
    void setContextMenu(bool contextMenu);
+   void setListLines(QGraphicsLineItem* line);
+   QList<QGraphicsLineItem*> getListLines();
+
+
+protected:
+   void mouseReleaseEvent(QGraphicsSceneMouseEvent *event);
+   void mousePressEvent(QGraphicsSceneMouseEvent *event);
+   QVariant itemChange(GraphicsItemChange change, const QVariant &value);
+
 
 private :
     Movement *movement;
     bool contextMenu;
+    BlackBoard* blackboard;
+    QList<QGraphicsLineItem*> listLines;
+
 
 
 };
